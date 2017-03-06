57e04db2ae076ae54552cc58e65667761dfcd358
indakilla <indakilla@mail.ru>
2017-03-06 14:38:19 +0200

    Initial commit

---
+++ app/src/main/java/englishlanguagepro/rodinagames/englishlessonslearnplaytest/IrregularVerbsActivity.java
@@ -0,0 +1,13 @@
+package englishlanguagepro.rodinagames.englishlessonslearnplaytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IrregularVerbsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_irregular_verbs);
+    }
+}
